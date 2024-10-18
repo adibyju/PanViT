23f78fcdc8afebb8bed798ca413338331d4db663
Aditya Byju <adityabyju7@gmail.com>
2024-10-18 21:54:22 +0530

    Update changes

--- src/backbones/swin.py
+++ src/backbones/swin.py
@@ -97,3 +97,419 @@ class UTAE_Swin(nn.Module):
             return out, maps
         return out
 
+
+class TemporallySharedBlock(nn.Module):
+    """
+    Helper module for convolutional encoding blocks that are shared across a sequence.
+    This module adds the self.smart_forward() method the the block.
+    smart_forward will combine the batch and temporal dimension of an input tensor
+    if it is 5-D and apply the shared convolutions to all the (batch x temp) positions.
+    """
+
+    def __init__(self, pad_value=None):
+        super(TemporallySharedBlock, self).__init__()
+        self.out_shape = None
+        self.pad_value = pad_value
+
+    def smart_forward(self, input):
+        if len(input.shape) == 4:
+            return self.forward(input)
+        else:
+            b, t, c, h, w = input.shape
+
+            if self.pad_value is not None:
+                dummy = torch.zeros(input.shape, device=input.device).float()
+                self.out_shape = self.forward(dummy.view(b * t, c, h, w)).shape
+
+            out = input.view(b * t, c, h, w)
+            if self.pad_value is not None:
+                pad_mask = (out == self.pad_value).all(dim=-1).all(dim=-1).all(dim=-1)
+                if pad_mask.any():
+                    temp = (
+                        torch.ones(
+                            self.out_shape, device=input.device, requires_grad=False
+                        )
+                        * self.pad_value
+                    )
+                    temp[~pad_mask] = self.forward(out[~pad_mask])
+                    out = temp
+                else:
+                    out = self.forward(out)
+            else:
+                out = self.forward(out)
+            _, c, h, w = out.shape
+            out = out.view(b, t, c, h, w)
+            return out
+
+
+class ConvLayer(nn.Module):
+    def __init__(
+        self,
+        nkernels,
+        norm="batch",
+        k=3,
+        s=1,
+        p=1,
+        n_groups=4,
+        last_relu=True,
+        padding_mode="reflect",
+    ):
+        super(ConvLayer, self).__init__()
+        layers = []
+        if norm == "batch":
+            nl = nn.BatchNorm2d
+        elif norm == "instance":
+            nl = nn.InstanceNorm2d
+        elif norm == "group":
+            nl = lambda num_feats: nn.GroupNorm(
+                num_channels=num_feats,
+                num_groups=n_groups,
+            )
+        else:
+            nl = None
+        for i in range(len(nkernels) - 1):
+            layers.append(
+                nn.Conv2d(
+                    in_channels=nkernels[i],
+                    out_channels=nkernels[i + 1],
+                    kernel_size=k,
+                    padding=p,
+                    stride=s,
+                    padding_mode=padding_mode,
+                )
+            )
+            if nl is not None:
+                layers.append(nl(nkernels[i + 1]))
+
+            if last_relu:
+                layers.append(nn.ReLU())
+            elif i < len(nkernels) - 2:
+                layers.append(nn.ReLU())
+        self.conv = nn.Sequential(*layers)
+
+    def forward(self, input):
+        return self.conv(input)
+
+
+class ConvBlock(TemporallySharedBlock):
+    def __init__(
+        self,
+        nkernels,
+        pad_value=None,
+        norm="batch",
+        last_relu=True,
+        padding_mode="reflect",
+    ):
+        super(ConvBlock, self).__init__(pad_value=pad_value)
+        self.conv = ConvLayer(
+            nkernels=nkernels,
+            norm=norm,
+            last_relu=last_relu,
+            padding_mode=padding_mode,
+        )
+
+    def forward(self, input):
+        return self.conv(input)
+
+
+class DownConvBlock(TemporallySharedBlock):
+    def __init__(
+        self,
+        d_in,
+        d_out,
+        k,
+        s,
+        p,
+        pad_value=None,
+        norm="batch",
+        padding_mode="reflect",
+    ):
+        super(DownConvBlock, self).__init__(pad_value=pad_value)
+        self.down = ConvLayer(
+            nkernels=[d_in, d_in],
+            norm=norm,
+            k=k,
+            s=s,
+            p=p,
+            padding_mode=padding_mode,
+        )
+        self.conv1 = ConvLayer(
+            nkernels=[d_in, d_out],
+            norm=norm,
+            padding_mode=padding_mode,
+        )
+        self.conv2 = ConvLayer(
+            nkernels=[d_out, d_out],
+            norm=norm,
+            padding_mode=padding_mode,
+        )
+
+    def forward(self, input):
+        out = self.down(input)
+        out = self.conv1(out)
+        out = out + self.conv2(out)
+        return out
+
+
+class UpConvBlock(nn.Module):
+    def __init__(
+        self, d_in, d_out, k, s, p, norm="batch", d_skip=None, padding_mode="reflect"
+    ):
+        super(UpConvBlock, self).__init__()
+        d = d_out if d_skip is None else d_skip
+        self.skip_conv = nn.Sequential(
+            nn.Conv2d(in_channels=d, out_channels=d, kernel_size=1),
+            nn.BatchNorm2d(d),
+            nn.ReLU(),
+        )
+        self.up = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels=d_in, out_channels=d_out, kernel_size=k, stride=s, padding=p
+            ),
+            nn.BatchNorm2d(d_out),
+            nn.ReLU(),
+        )
+        self.conv1 = ConvLayer(
+            nkernels=[d_out + d, d_out], norm=norm, padding_mode=padding_mode
+        )
+        self.conv2 = ConvLayer(
+            nkernels=[d_out, d_out], norm=norm, padding_mode=padding_mode
+        )
+
+    def forward(self, input, skip):
+        out = self.up(input)
+        out = torch.cat([out, self.skip_conv(skip)], dim=1)
+        out = self.conv1(out)
+        out = out + self.conv2(out)
+        return out
+
+
+class Temporal_Aggregator(nn.Module):
+    def __init__(self, mode="mean"):
+        super(Temporal_Aggregator, self).__init__()
+        self.mode = mode
+
+    def forward(self, x, pad_mask=None, attn_mask=None):
+        if pad_mask is not None and pad_mask.any():
+            if self.mode == "att_group":
+                n_heads, b, t, h, w = attn_mask.shape
+                attn = attn_mask.view(n_heads * b, t, h, w)
+
+                if x.shape[-2] > w:
+                    attn = nn.Upsample(
+                        size=x.shape[-2:], mode="bilinear", align_corners=False
+                    )(attn)
+                else:
+                    attn = nn.AvgPool2d(kernel_size=w // x.shape[-2])(attn)
+
+                attn = attn.view(n_heads, b, t, *x.shape[-2:])
+                attn = attn * (~pad_mask).float()[None, :, :, None, None]
+
+                out = torch.stack(x.chunk(n_heads, dim=2))  # hxBxTxC/hxHxW
+                out = attn[:, :, :, None, :, :] * out
+                out = out.sum(dim=2)  # sum on temporal dim -> hxBxC/hxHxW
+                out = torch.cat([group for group in out], dim=1)  # -> BxCxHxW
+                return out
+            elif self.mode == "att_mean":
+                attn = attn_mask.mean(dim=0)  # average over heads -> BxTxHxW
+                attn = nn.Upsample(
+                    size=x.shape[-2:], mode="bilinear", align_corners=False
+                )(attn)
+                attn = attn * (~pad_mask).float()[:, :, None, None]
+                out = (x * attn[:, :, None, :, :]).sum(dim=1)
+                return out
+            elif self.mode == "mean":
+                out = x * (~pad_mask).float()[:, :, None, None, None]
+                out = out.sum(dim=1) / (~pad_mask).sum(dim=1)[:, None, None, None]
+                return out
+        else:
+            if self.mode == "att_group":
+                n_heads, b, t, h, w = attn_mask.shape
+                attn = attn_mask.view(n_heads * b, t, h, w)
+                if x.shape[-2] > w:
+                    attn = nn.Upsample(
+                        size=x.shape[-2:], mode="bilinear", align_corners=False
+                    )(attn)
+                else:
+                    attn = nn.AvgPool2d(kernel_size=w // x.shape[-2])(attn)
+                attn = attn.view(n_heads, b, t, *x.shape[-2:])
+                out = torch.stack(x.chunk(n_heads, dim=2))  # hxBxTxC/hxHxW
+                out = attn[:, :, :, None, :, :] * out
+                out = out.sum(dim=2)  # sum on temporal dim -> hxBxC/hxHxW
+                out = torch.cat([group for group in out], dim=1)  # -> BxCxHxW
+                return out
+            elif self.mode == "att_mean":
+                attn = attn_mask.mean(dim=0)  # average over heads -> BxTxHxW
+                attn = nn.Upsample(
+                    size=x.shape[-2:], mode="bilinear", align_corners=False
+                )(attn)
+                out = (x * attn[:, :, None, :, :]).sum(dim=1)
+                return out
+            elif self.mode == "mean":
+                return x.mean(dim=1)
+
+
+class RecUNet(nn.Module):
+    """Recurrent U-Net architecture. Similar to the U-TAE architecture but
+    the L-TAE is replaced by a recurrent network
+    and temporal averages are computed for the skip connections."""
+
+    def __init__(
+        self,
+        input_dim,
+        encoder_widths=[64, 64, 64, 128],
+        decoder_widths=[32, 32, 64, 128],
+        out_conv=[32, 20],
+        str_conv_k=4,
+        str_conv_s=2,
+        str_conv_p=1,
+        temporal="lstm",
+        input_size=128,
+        encoder_norm="group",
+        hidden_dim=128,
+        encoder=False,
+        padding_mode="reflect",
+        pad_value=0,
+    ):
+        super(RecUNet, self).__init__()
+        self.n_stages = len(encoder_widths)
+        self.temporal = temporal
+        self.encoder_widths = encoder_widths
+        self.decoder_widths = decoder_widths
+        self.enc_dim = (
+            decoder_widths[0] if decoder_widths is not None else encoder_widths[0]
+        )
+        self.stack_dim = (
+            sum(decoder_widths) if decoder_widths is not None else sum(encoder_widths)
+        )
+        self.pad_value = pad_value
+
+        self.encoder = encoder
+        if encoder:
+            self.return_maps = True
+        else:
+            self.return_maps = False
+
+        if decoder_widths is not None:
+            assert len(encoder_widths) == len(decoder_widths)
+            assert encoder_widths[-1] == decoder_widths[-1]
+        else:
+            decoder_widths = encoder_widths
+
+        self.in_conv = ConvBlock(
+            nkernels=[input_dim] + [encoder_widths[0], encoder_widths[0]],
+            pad_value=pad_value,
+            norm=encoder_norm,
+        )
+
+        self.down_blocks = nn.ModuleList(
+            DownConvBlock(
+                d_in=encoder_widths[i],
+                d_out=encoder_widths[i + 1],
+                k=str_conv_k,
+                s=str_conv_s,
+                p=str_conv_p,
+                pad_value=pad_value,
+                norm=encoder_norm,
+                padding_mode=padding_mode,
+            )
+            for i in range(self.n_stages - 1)
+        )
+        self.up_blocks = nn.ModuleList(
+            UpConvBlock(
+                d_in=decoder_widths[i],
+                d_out=decoder_widths[i - 1],
+                d_skip=encoder_widths[i - 1],
+                k=str_conv_k,
+                s=str_conv_s,
+                p=str_conv_p,
+                norm=encoder_norm,
+                padding_mode=padding_mode,
+            )
+            for i in range(self.n_stages - 1, 0, -1)
+        )
+        self.temporal_aggregator = Temporal_Aggregator(mode="mean")
+
+        if temporal == "mean":
+            self.temporal_encoder = Temporal_Aggregator(mode="mean")
+        elif temporal == "lstm":
+            size = int(input_size / str_conv_s ** (self.n_stages - 1))
+            self.temporal_encoder = ConvLSTM(
+                input_dim=encoder_widths[-1],
+                input_size=(size, size),
+                hidden_dim=hidden_dim,
+                kernel_size=(3, 3),
+            )
+            self.out_convlstm = nn.Conv2d(
+                in_channels=hidden_dim,
+                out_channels=encoder_widths[-1],
+                kernel_size=3,
+                padding=1,
+            )
+        elif temporal == "blstm":
+            size = int(input_size / str_conv_s ** (self.n_stages - 1))
+            self.temporal_encoder = BConvLSTM(
+                input_dim=encoder_widths[-1],
+                input_size=(size, size),
+                hidden_dim=hidden_dim,
+                kernel_size=(3, 3),
+            )
+            self.out_convlstm = nn.Conv2d(
+                in_channels=2 * hidden_dim,
+                out_channels=encoder_widths[-1],
+                kernel_size=3,
+                padding=1,
+            )
+        elif temporal == "mono":
+            self.temporal_encoder = None
+        self.out_conv = ConvBlock(nkernels=[decoder_widths[0]] + out_conv, padding_mode=padding_mode)
+
+    def forward(self, input, batch_positions=None):
+        pad_mask = (
+            (input == self.pad_value).all(dim=-1).all(dim=-1).all(dim=-1)
+        )  # BxT pad mask
+
+        out = self.in_conv.smart_forward(input)
+
+        feature_maps = [out]
+        # ENCODER
+        for i in range(self.n_stages - 1):
+            out = self.down_blocks[i].smart_forward(feature_maps[-1])
+            feature_maps.append(out)
+
+        # Temporal encoder
+        if self.temporal == "mean":
+            out = self.temporal_encoder(feature_maps[-1], pad_mask=pad_mask)
+        elif self.temporal == "lstm":
+            _, out = self.temporal_encoder(feature_maps[-1], pad_mask=pad_mask)
+            out = out[0][1]  # take last cell state as embedding
+            out = self.out_convlstm(out)
+        elif self.temporal == "blstm":
+            out = self.temporal_encoder(feature_maps[-1], pad_mask=pad_mask)
+            out = self.out_convlstm(out)
+        elif self.temporal == "mono":
+            out = feature_maps[-1]
+
+        if self.return_maps:
+            maps = [out]
+        for i in range(self.n_stages - 1):
+            if self.temporal != "mono":
+                skip = self.temporal_aggregator(
+                    feature_maps[-(i + 2)], pad_mask=pad_mask
+                )
+            else:
+                skip = feature_maps[-(i + 2)]
+            out = self.up_blocks[i](out, skip)
+            if self.return_maps:
+                maps.append(out)
+
+        if self.encoder:
+            return out, maps
+        else:
+            out = self.out_conv(out)
+            if self.return_maps:
+                return out, maps
+            else:
+                return out
+
+
